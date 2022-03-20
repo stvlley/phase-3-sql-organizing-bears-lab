5a131fa37664f1c294ce62f8ff430994bfd541cc
Stephen Talley <stvlley.amzn@gmail.com>
2022-03-20 17:58:37 -0400

    commit tree 24c3a064ae68884cc5a77ce1f02c9a5fc834387f

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name varchar(255),
+    Age int,
+    Sex varchar(255),
+    Color varchar(255),
+    temperament varchar(255),
+    Alive bool
+);
